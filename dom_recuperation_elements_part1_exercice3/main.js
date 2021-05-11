02d25eaaf92415e5fcfc7aae235d34be4907cfd7
KrispyTea <80962605+KrispyTea@users.noreply.github.com>
2021-05-11 11:13:17 +0200

    over

---
+++ dom_recuperation_elements_part1_exercice3/main.js
@@ -0,0 +1,13 @@
+let a = document.getElementsByTagName("div")[2]
+let a1 = a.firstElementChild
+console.log(a1);
+
+let a2 = a.lastElementChild
+console.log(a2);
+
+let i = document.getElementsByTagName("i")
+console.log(i);
+
+let b = document.getElementsByTagName("b")
+console.log(b);
+
